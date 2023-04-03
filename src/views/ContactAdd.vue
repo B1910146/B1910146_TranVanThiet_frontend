0d1840e341464accae88968753e66a4631add4d0
B1910146 <thietb1910146@student.ctu.edu.vn>
2023-04-03 22:14:11 +0700

    Tao trang them moi lien he

---
+++ src/views/ContactAdd.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="page">
+        <h4>Thêm Liên hệ mới</h4>
+        <ContactForm :contact={} @submit:contact="addContact" />
+        <p>{{ message }}</p>
+    </div>
+</template>
+<script>
+import ContactForm from "@/components/ContactForm.vue";
+import ContactService from "@/services/contact.service";
+export default {
+    components: {
+        ContactForm,
+    },
+    data() {
+        return {
+            contact: null,
+            message: "",
+        };
+    },
+    methods: {
+        async addContact(data) {
+            try {
+                await ContactService.create(data);
+                this.message = "Liên hệ được thêm mới thành công.";
+            } catch (error) {
+                console.log(error);
+            }
+        },
+    },
+};
+</script>
